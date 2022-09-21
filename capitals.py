116a11e5f67b51eedab0d2779113e18970197abc
Kenny Lai <kennylai@Kennys-MacBook-Pro.local>
2022-09-21 17:04:32 +1000

    capitals module initial commit

---
+++ capitals.py
@@ -0,0 +1,27 @@
+def capitals():
+
+
+    option_1 =''
+    option_2 =''
+    option_3 =''
+    option_4 =''
+
+    country = 'Australia'
+
+
+
+
+
+
+
+
+    print(f'Select the capital of {country}')
+
+
+
+
+
+
+
+
+capitals()
