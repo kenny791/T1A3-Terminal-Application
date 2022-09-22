06c6450426a3bb3d5d7cea1aa80641ef3501b267
Kenny Lai <kennylai@Kennys-MacBook-Pro.local>
2022-09-22 16:14:03 +1000

    main - cleaned up code to remove redundant lines

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 # import os
 import clearing
 # from re import A
-from pycountry import countries
-import pycountry
-from countryinfo import CountryInfo
-import list
-import json
+# from pycountry import countries
+# import pycountry
+# from countryinfo import CountryInfo
+# import list
+# import json
 from pick import pick
-import time
-import random
+# import time
+# import random
 from colorama import init
-from colorama import Fore, Back, Style
+from colorama import Fore
 from lookup import lookup
-
 from name_countries import name_countries
-
+from capitals import capitals
 
 
 
@@ -31,27 +30,23 @@ input("\n \t \t \t\t Press enter to continue")
 
 
 
-# Menu selection screen
+
 def menu():
+    '''Menu selection screen'''
     title = 'Please choose an option and press enter: '
-    options = ['Name the Countries', 'Menu2', 'Exit']
+    options = ['Name the Countries', 'Learn the capitals', 'Exit']
     option, index = pick(options, title)
     if option == 'Name the Countries':
-        clearing.clear()
-        print(Fore.BLACK+''' ---NAME THE COUNTRIES---
-    Welcome to the Name the countries training module. 
-    Here you can test your skills by naming all the countries starting with the letter prompted. You will have 3 chances for any mistakes. Have fun!
-        ''')
-        input("Press enter to continue")
-        clearing.clear()
+
 
         name_countries()
-        exit()
+
         SystemExit
-    elif option == 'Menu2':
-        pass
+    elif option == 'Learn the capitals':
+        capitals()
 
     elif option == 'Country Information Lookup':
+        lookup()
         clearing.clear()
         lookup()
     elif option == 'Exit':
@@ -61,68 +56,3 @@ def menu():
 
 
 menu()
-
-
-
-
-
-
-
-
-
-# def app_controller():
-#     choice =input('\nPress Enter to search again: \n[m] to go back to menu \n[q] to quit the app\n')
-
-#     if choice == 'm':
-#         pass
-
-#     if choice == 'q':
-#         SystemExit
-
-
-
-
-
-
-
-
-
-# # selection screen
-
-# menu = {}
-# menu['1']="Option 1" 
-# menu['2']="Option 2"
-# menu['3']="Option 3"
-# menu['4']="Exit"
-
-
-
-
-
-
-
-# while True: 
-#     options=menu.keys() 
-#     for entry in options: 
-#       print (entry, menu[entry])
-
-#     selection=input("Please Select:") 
-#     if selection =='1': 
-#       print ("add") 
-#     elif selection == '2': 
-#       print ("delete")
-#     elif selection == '3':
-#       print ("find") 
-#     elif selection == '4': 
-#       break
-#     else: 
-#       print ("Unknown Option Selected!")
-
-
-
-
-
-
-
-
-
