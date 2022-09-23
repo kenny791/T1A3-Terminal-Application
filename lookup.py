2af8ca4a6ddfacc732c69222d767282f5fdc8199
Kenny Lai <kennylai@Kennys-MacBook-Pro.local>
2022-09-23 22:19:54 +1000

    lookup - add try except for country searcg

--- lookup.py
+++ lookup.py
@@ -13,25 +13,31 @@ def menu():
 def lookup():
 
     clearing.clear()
-    searchcountry = input('Please enter country name for more information: ')
-    if searchcountry == '':
-        lookup()
+    try:
+        searchcountry = input('Please enter country name for more information: ')
+        if searchcountry == '':
+            lookup()
 
 
-    country = CountryInfo(searchcountry)
+        country = CountryInfo(searchcountry)
 
-    print(f'Subregion\t{country.subregion()}')
-    print(f'Demonym\t\t{country.demonym()}')
-    print(f'Population\t{country.population()}')
-    print('Currency \t', end="")
-    print( *country.currencies(),sep=',')
-    print('Time Zone \t', end="")
-    print(*country.timezones(),sep=',')
-    print(f'Area\t\t{country.area()} sqkm')
-    print('Calling Code \t+', end="")
-    print(*country.calling_codes(),sep=',')
-    print('\nWould you like to search another country')
+        print(f'Subregion\t{country.subregion()}')
+        print(f'Demonym\t\t{country.demonym()}')
+        print(f'Population\t{country.population()}')
+        print('Currency \t', end="")
+        print( *country.currencies(),sep=',')
+        print('Time Zone \t', end="")
+        print(*country.timezones(),sep=',')
+        print(f'Area\t\t{country.area()} sqkm')
+        print('Calling Code \t+', end="")
+        print(*country.calling_codes(),sep=',')
+        print('\nWould you like to search another country')
     
+    except:
+        print('\nCountry not found, please enter another country\n')
+
+
+
     while True:
         print('\nPress enter to continue...')
         print('or [m] to go back to the main menu.')
